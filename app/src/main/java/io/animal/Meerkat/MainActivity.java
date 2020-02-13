433b2af53c98e356a541c16c07d79338c8425866
shseo@digicaps.com <shseo@digicaps.com>
2020-02-13 18:37:37 +0900

    [backup]

--- app/src/main/java/io/animal/Meerkat/MainActivity.java
+++ app/src/main/java/io/animal/Meerkat/MainActivity.java
@@ -2,11 +2,24 @@ package io.animal.Meerkat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import io.animal.Meerkat.ui.main.MainFragment;
+import io.animal.Meerkat.util.PermissionHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static int REQ_CODE_OVERLAY_PERMISSION = 101;
+
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +31,46 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.main_container, new MainFragment())
                     .commitNow();
         }
+
+        // bind UI resource
+        floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // check floating permission
+                PermissionHelper permissionHelper = new PermissionHelper(getApplicationContext());
+                if (!permissionHelper.hasSystemAlertWindows()) {
+                    showAlertPermission();
+                    return;
+                }
+
+                // show floating view
+
+            }
+        });
+    }
+
+    private void showAlertPermission() {
+        new MaterialAlertDialogBuilder(this)
+                .setMessage("서버시계를 화면에 띄우기 위해서는 '플로팅' 권한이 필요합니다.")
+                .setPositiveButton("예", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                                Uri.parse("package:" + getApplicationContext().getPackageName()));
+                        startActivityForResult(intent, REQ_CODE_OVERLAY_PERMISSION);
+                    }
+                })
+                .setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
+    private void enableClockFloatingView()  {
+
     }
 }
---
+++ app/src/main/java/io/animal/Meerkat/util/PermissionHelper.java
@@ -0,0 +1,23 @@
+package io.animal.Meerkat.util;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Build;
+import android.provider.Settings;
+
+public class PermissionHelper extends ContextWrapper {
+
+    public PermissionHelper(Context c) {
+        super(c);
+    }
+
+    public boolean hasSystemAlertWindows() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        if (!Settings.canDrawOverlays(getApplicationContext())) {
+            return false;
+        }
+        return true;
+    }
+}
